273ca31cbe9b4dbf774d2d9cec2eb152fadfa890
sicaolong163 <39057759+sicaolong163@users.noreply.github.com>
2018-10-06 21:50:41 +0800

    Create 009举行覆盖.cpp

---
+++ 009举行覆盖.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int rectCover(int number)
+    {
+        vector<int>dp(number+1,0);
+        dp[1]=1;
+        dp[2]=2;
+        for(int i=3;i<=number;i++)
+            dp[i]=dp[i-1]+dp[i-2];
+        return dp[number];
+    }
+};
