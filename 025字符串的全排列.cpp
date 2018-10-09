8a2cadfaaa73b275d6749f2a295556eefed62a68
sicaolong163 <39057759+sicaolong163@users.noreply.github.com>
2018-10-09 17:59:21 +0800

    Update and rename 025字符串的全排列 to 025字符串的全排列.cpp

--- 025字符串的全排列.cpp
+++ 025字符串的全排列.cpp
@@ -3,18 +3,18 @@ private:
     vector<string>result;
     void dfs(string &str,int start,int end)
     {
-        if(start==end)
+        if(start==end)//终止的条件
         {
             result.push_back(str);
             return ;
         }
-        for(int i=start;i<=end;i++)
+        for(int i=start;i<=end;i++)//
         {
             if(i!=start&&str[i]==str[start])
                 continue;
-            swap(str[i],str[start]);
-            dfs(str,start+1,end);
-            swap(str[i],str[start]);
+            swap(str[i],str[start]);//处理
+            dfs(str,start+1,end);//递归
+            swap(str[i],str[start]);//回溯
         }
         return ;
     }
